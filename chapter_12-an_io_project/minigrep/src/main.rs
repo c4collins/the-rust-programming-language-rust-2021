f30dcdd1ecbb7e17d335cbd8da1178b8bef876a5
Connor Collins <connor.collins@gmail.com>
2023-07-24 15:47:27 -0400

    Chapter 12 complete

--- chapter_12-an_io_project/minigrep/src/main.rs
+++ chapter_12-an_io_project/minigrep/src/main.rs
@@ -4,7 +4,7 @@ use std::{env, process};
 fn main() {
     let args: Vec<String> = env::args().collect();
     let config = minigrep::configuration::Config::new(&args).unwrap_or_else(|err| {
-        println!("Problem passing arguments: {}", err);
+        eprintln!("Problem passing arguments: {}", err);
         process::exit(1)
     });
 
@@ -12,7 +12,7 @@ fn main() {
     // println!("In file {}", config.file_path);
 
     if let Err(e) = minigrep::run(config) {
-        println!("Application error: {e}");
+        eprintln!("Application error: {e}");
         process::exit(1);
     }
 }
